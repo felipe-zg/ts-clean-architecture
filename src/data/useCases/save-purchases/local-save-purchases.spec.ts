51b0e669505b6298819357dd10337891f34d617f
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-08-21 11:28:59 -0300

    test: ensure LocalSavePurchases creates cache with correct timestamp

--- src/data/useCases/save-purchases/local-save-purchases.spec.ts
+++ src/data/useCases/save-purchases/local-save-purchases.spec.ts
@@ -6,9 +6,9 @@ type SutTypes = {
   cacheStore: CacheStoreSpy;
 };
 
-const makeSut = (): SutTypes => {
+const makeSut = (timestamp = new Date()): SutTypes => {
   const cacheStore = new CacheStoreSpy();
-  const sut = new LocalSavePurchases(cacheStore);
+  const sut = new LocalSavePurchases(cacheStore, timestamp);
   return {
     sut,
     cacheStore,
@@ -30,7 +30,8 @@ describe("localSavePurchases", () => {
   });
 
   it("should insert new cache if delete succeeds", async () => {
-    const { cacheStore, sut } = makeSut();
+    const timestamp = new Date();
+    const { cacheStore, sut } = makeSut(timestamp);
     const purchases = mockPurchases();
     await sut.save(purchases);
     expect(cacheStore.messages).toEqual([
@@ -39,7 +40,10 @@ describe("localSavePurchases", () => {
     ]);
     expect(cacheStore.insertKey).toBe("purchases");
     expect(cacheStore.deleteKey).toBe("purchases");
-    expect(cacheStore.insertValues).toEqual(purchases);
+    expect(cacheStore.insertValues).toEqual({
+      timestamp,
+      value: purchases,
+    });
   });
 
   it("should throw if insert throws", async () => {
